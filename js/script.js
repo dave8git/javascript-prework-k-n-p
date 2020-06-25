1e29eb9fefdb280fc9e9c123a95934ffb5f5c080
davepol <dave8git@gmail.com>
2020-06-25 21:50:10 +0200

    fix variables to let and const insstead of var

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-var argButtonName, buttonPaper, buttonRock, buttonScissors, buttonTest;
-
-buttonPaper = document.getElementById('button-paper');
-buttonPaper.addEventListener('click', function () {
+const buttonPaper = document.getElementById('button-paper');
+buttonPaper.addEventListener('click', function() {
     buttonClicked('papier');
 });
-buttonRock = document.getElementById('button-rock');
-buttonRock.addEventListener('click', function () {
+const buttonRock = document.getElementById('button-rock');
+buttonRock.addEventListener('click', function() {
     buttonClicked('kamień');
 });
-buttonScissors = document.getElementById('button-scissors');
-buttonScissors.addEventListener('click', function () {
+const buttonScissors = document.getElementById('button-scissors');
+buttonScissors.addEventListener('click', function() {
     buttonClicked('nożyce');
 });
+
+
 
 function buttonClicked(argButtonName) {
     clearMessages();
     console.log(argButtonName + ' został kliknięty');
-
-    var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
 
     function getMoveName(argMoveId) {
         console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
@@ -49,17 +47,15 @@ function buttonClicked(argButtonName) {
         printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
     }
 
-
-    argPlayerMove;
     //playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-    console.log('wybór ruchu gracza to: ' + playerInput);
+    //console.log('wybór ruchu gracza to: ' + playerInput);
     //playerMove = getMoveName(playerInput);
-    playerMove = argButtonName;
+    const playerMove = argButtonName;
 
     console.log('ruch gracza to: ' + playerMove);
-    randomNumber = Math.floor(Math.random() * 3 + 1);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('wylosowana liczba to: ' + randomNumber);
-    computerMove = getMoveName(randomNumber);
+    const computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
     displayResult(playerMove, computerMove);
 }
